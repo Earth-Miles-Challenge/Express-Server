7aaaeda21432afe5ed1b8d9a4f7cddd80a8e2877
Eric Daams <eric@164a.com>
2022-10-06 15:31:47 +0930

    Trying a different tack. Redirect to the /auth/strava URL from Strava and getting the client token there. Then need to store the token & athlete details for use in client.

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,35 @@
 const axios = require('axios');
 const express = require('express');
 const router = express.Router();
-const {_clientId, _clientSecret} = require('../utils/strava.js');
+const { getClientToken } = require('../utils/strava.js');
 
 /* GET home page. */
+router.get('/strava', async (req, res, next) => {
+	const redirect = req.query.state || 'http://localhost:3000'
+
+	if (!req.query.code || !req.query.scope) {
+		res.send('missing codes');
+	}
+
+	const response = await getClientToken(req.query.code);
+
+	if (response) {
+		res.json(response);
+	}
+
+	const err = new Error();
+	err.status = 500;
+	next(err);
+	// res.redirect(redirect + '?success');
+
+	// .then((response) => {
+	// 	res.json(response.athlete);
+	// })
+	// .catch((err) => {
+	// 	next(err);
+	// });
+});
+
 router.post('/strava', function(req, res, next) {
 	if (!req.body.code || req.body.code.length === 0) {
 		const err = new Error('Missing Strava code');
@@ -18,7 +44,6 @@ router.post('/strava', function(req, res, next) {
 		grant_type: 'authorization_code'
 	})
 	.then((response) => {
-		console.log(response);
 		res.json(response.athlete);
 	})
 	.catch((err) => {
--- utils/strava.js
+++ utils/strava.js
@@ -1,7 +1,24 @@
+const axios = require('axios');
 const fs = require('fs');
 const keys = require('../../private/keys.json');
 
+const getClientToken = async (code) => {
+	try {
+		const response = await axios.post(`https://www.strava.com/api/v3/oauth/token`, {
+			client_id: keys.stravaClientId,
+			client_secret: keys.stravaClientSecret,
+			code: code,
+			grant_type: 'authorization_code'
+		});
+		console.log(response);
+		return response.data;
+	} catch (err) {
+		console.log(err);
+		return false;
+	}
+}
+
 module.exports = {
-	_clientId: keys.stravaClientId,
-	_clientSecret: keys.stravaClientSecret,
+	clientId: keys.stravaClientId,
+	getClientToken
 }
