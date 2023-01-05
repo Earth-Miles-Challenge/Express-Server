223e641f0659b875b8f2d38110eedebea07cd11c
Eric Daams <eric@164a.com>
2023-01-05 21:07:43 +0930

    Tweaked IP block and commented out a couple user routes which are not used.

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -16,9 +16,9 @@ router.get('/:userId/activities/fetchLatest', activitiesController.fetchLatest);
 router.get('/:userId/activities/:activityId', activitiesController.activityExists, activitiesController.getOne);
 
 // Base
-router.get('/', usersController.get);
+// router.get('/', usersController.get);
 router.get('/:userId', usersController.getOne);
-router.post('/', usersController.create);
+// router.post('/', usersController.create);
 router.put('/:userId', usersController.update);
 router.get('/:userId/onboard', usersController.onboard);
 router.delete('/:userId', usersController.remove);
